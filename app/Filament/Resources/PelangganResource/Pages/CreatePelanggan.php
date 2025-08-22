874717d47568ea095f0ff84c11810574eb80cd91
fauzanalhfzh <zenbangga@gmail.com>
2025-08-22 11:03:55 +0700

    adding mutate for pelanggan create

--- app/Filament/Resources/PelangganResource/Pages/CreatePelanggan.php
+++ app/Filament/Resources/PelangganResource/Pages/CreatePelanggan.php
@@ -5,10 +5,20 @@ namespace App\Filament\Resources\PelangganResource\Pages;
 use App\Filament\Resources\PelangganResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Hash;
 
 class CreatePelanggan extends CreateRecord
 {
     protected static string $resource = PelangganResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (!empty($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+        return $data;
+    }
+
 
     protected function getRedirectUrl(): string
     {
