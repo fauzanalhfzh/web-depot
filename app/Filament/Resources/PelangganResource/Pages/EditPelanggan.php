2bfe172209002e2ae78ef4ea30d251fdfd4fd937
fauzanalhfzh <zenbangga@gmail.com>
2025-08-22 17:14:00 +0700

    fix bugs

--- app/Filament/Resources/PelangganResource/Pages/EditPelanggan.php
+++ app/Filament/Resources/PelangganResource/Pages/EditPelanggan.php
@@ -5,10 +5,19 @@ namespace App\Filament\Resources\PelangganResource\Pages;
 use App\Filament\Resources\PelangganResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Hash;
 
 class EditPelanggan extends EditRecord
 {
     protected static string $resource = PelangganResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (!empty($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+        return $data;
+    }
 
     protected function getHeaderActions(): array
     {
